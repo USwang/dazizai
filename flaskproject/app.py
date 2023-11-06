b8ba86fa59424c231a9148428073554c9a51ff15
wsy <wangshiyu217@163.com>
2023-11-06 16:25:34 +0800

    database ok

--- flaskproject/app.py
+++ flaskproject/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template, request, url_for
 import config
 from exts import db
-from models import Stockdata
+from models import Stockdata, Stocklist
 from flask_migrate import Migrate
+from eastmoneystockdataget import getjson_stockdata
+from eastmoneystocklistget import getjson_stocklist, pageNumber
+
 app = Flask(__name__)
 app.config.from_object(config)
 db.init_app(app)
@@ -11,16 +14,77 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    stockdata = Stockdata(id=666666, stockname='大自在', markettag='hu', datajson={'key': 'value'})
-    db.session.add(stockdata)
-    db.session.commit()
     return 'Hello Wo'
 
 
-@app.route('/refresh/')
-def refresh():
-    return 'refresh finished!'
+@app.route('/update/stockdata/',methods=['GET','POST'])
+def update_stockdata():
+    if request.method == 'GET':
+        return render_template('stockdata.html')
+    else:
+        # 验证密码
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # 更新数据库
+        if username == 'wsy'and password == 'mtjb1..':
+            lies = db.session.query(Stocklist).all()
+            for li in lies:
+                SECURITY_CODE = li.SECURITY_CODE
+                SECURITY_NAME_ABBR = li.SECURITY_NAME_ABBR
+                da = db.session.query(Stockdata).filter_by(SECURITY_CODE=SECURITY_CODE).first()
+                datajson = getjson_stockdata(SECURITY_CODE)
+                if da:
+                    da.datajson=datajson
+                    db.session.commit()
+                else:
+                    stockdata = Stockdata(SECURITY_CODE=SECURITY_CODE, SECURITY_NAME_ABBR=SECURITY_NAME_ABBR, datajson=datajson)
+                    db.session.add(stockdata)
+                    db.session.commit()
+        else:
+            return '用户名或密码不正确'
+    return 'update finished'
 
 
+@app.route('/update/stocklist/',methods=['GET', 'POST'])
+def update_stocklist():
+    if request.method == 'GET':
+        return render_template('stocklist.html')
+    else:
+        # 验证密码
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # 更新数据库
+        if username == 'wsy' and password == 'mtjb1..':
+            totolPage = pageNumber()
+            for page in range(totolPage):
+                list_pages = getjson_stocklist(page + 1)
+                for lists_page in list_pages:
+                    SECURITY_CODE = lists_page['SECURITY_CODE']
+                    SECURITY_NAME_ABBR = lists_page['SECURITY_NAME_ABBR']
+                # 判断是否已经在数据库，
+                li = db.session.query(Stocklist).filter_by(SECURITY_CODE=SECURITY_CODE).first()
+                # print(bool(li))
+                if li:
+                    li.SECURITY_CODE = SECURITY_CODE
+                    li.SECURITY_NAME_ABBR = SECURITY_NAME_ABBR
+                    db.session.commit()
+                else:
+                    stocklist = Stocklist(SECURITY_CODE=SECURITY_CODE, SECURITY_NAME_ABBR=SECURITY_NAME_ABBR)
+                    db.session.add(stocklist)
+                    db.session.commit()
+        else:
+            return '用户名或密码不正确'
+    return 'update finished'
+
+
+# @app.route('/search')
+# def search():
+#     username = request.args.get('username')  # 从请求参数中获取要搜索的用户名
+#     users = User.query.filter_by(username=username).all()  # 使用 SQLAlchemy 查询该用户名的所有用户数据
+#
+#     if users:
+#         return render_template('search.html', users=users)  # 渲染搜索结果页面，并传递用户数据
+#     else:
+#         return "没有找到相关用户"  # 如果未找到用户，返回相应的提示信息
 if __name__ == '__main__':
     app.run()
--- flaskproject/models.py
+++ flaskproject/models.py
@@ -4,10 +4,22 @@ from exts import db
 class Stockdata(db.Model):
     __tablename__ = 'stock_data'
     # stock 代码
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # stock code number
+    SECURITY_CODE = db.Column(db.String(50), unique=True, nullable=True)
     # stock 名称
-    stockname = db.Column(db.String(50), unique=True, nullable=True)
+    SECURITY_NAME_ABBR = db.Column(db.String(50), nullable=True)
     # 市场标签 沪，深，创，科创
-    markettag = db.Column(db.String(50), nullable=True)
+    # markettag = db.Column(db.String(50), nullable=True)
     # 数据
     datajson = db.Column(db.JSON, nullable=True)
+
+class Stocklist(db.Model):
+    __tablename__ = 'stock_list'
+    # stock 代码
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    # stock code number
+    SECURITY_CODE = db.Column(db.String(50),unique=True,nullable=True)
+    # stock 名称
+    SECURITY_NAME_ABBR = db.Column(db.String(50), nullable=True)
+
